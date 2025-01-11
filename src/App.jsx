3cd1cba4a485cd18fa21e34998c331cde349d153
Adam-Elmi <Adamcade123@gmail.com>
2025-01-12 01:32:20 +0300

    Modified src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
-import StaticHighlight from "./Highlight/StaticHighlight";
+import Highlight from "./Highlight/Highlight";
+import "prismjs/themes/prism-atom-dark.css";
 
 import "./App.css";
 
 function App() {
   return (
     <>
-    <h1>Hello Adam</h1>
-      <StaticHighlight
-      codeBlock={`const greet = "Hello, Prism.js!";\nconsole.log(greet);`}
-      title="Javascript"
-      containerStyles={{borderRadius: "10px", overflow: "hidden", margin: "10px"}}
-    />
-      <StaticHighlight
-      codeBlock={`const greet = "Hello, Prism.js!";\nconsole.log(greet);`}
-      title="Javascript"
-      containerStyles={{borderRadius: "10px", overflow: "hidden", margin: "10px"}}
-    />
+      <h1>Hello Adam</h1>
+      <Highlight
+        codeBlock={`const greet = "Hello, Prism.js!";\nconsole.log(greet);`}
+        title="Javascript"
+        containerStyles={{
+          borderRadius: "10px",
+          overflow: "hidden",
+          margin: "10px",
+        }}
+        preStyles={{ backgroundColor: "#222" }}
+      />
+      <Highlight
+        codeBlock={`const greet = "Hello, Prism.js!";\nconsole.log(greet);`}
+        title="Javascript"
+        containerStyles={{
+          borderRadius: "10px",
+          overflow: "hidden",
+          margin: "10px",
+        }}
+        preStyles={{ backgroundColor: "#222" }}
+      />
     </>
   );
 }
