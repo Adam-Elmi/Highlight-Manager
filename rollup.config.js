f4e10b41f76693e368892c1737eacf3644e5814a
Adam-Elmi <Adamcade123@gmail.com>
2025-01-12 03:36:44 +0300

    Added rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
+export default {
+  input: 'src/Highlight.jsx', // Specify your main entry point here
+  output: [
+    {
+      file: 'dist/index.cjs.js', // CommonJS bundle
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.esm.js', // ES Module bundle
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(), // Exclude React and React-DOM from the bundle
+    resolve({ extensions: ['.js', '.jsx'] }), // Support .jsx files
+    commonjs(), // Convert CommonJS to ES6
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
+      presets: ['@babel/preset-react'], // Transpile React JSX
+      extensions: ['.js', '.jsx'], // Process both .js and .jsx files
+    }),
+  ],
+};
